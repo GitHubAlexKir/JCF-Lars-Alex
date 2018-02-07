23a3880ecf6c139fbe85c1b4be180eaa4a1be5d8
Lars Janssen <larsjanssen64@gmail.com>
2018-02-07 12:40:23 +0100

    Init test

---
+++ tests/unit/TestLessonOne.java
@@ -0,0 +1,19 @@
+package unit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LessonOne {
+
+    @Before
+    public void setup()
+    {
+
+    }
+
+    @Test
+    public void testAantalAction()
+    {
+
+    }
+}
